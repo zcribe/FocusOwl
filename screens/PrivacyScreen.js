ad96a80337d41ce9c662c7077f29bf0511a329f0
zcribe <erlend.eelmets@gmail.com>
2019-12-30 13:12:51 +0200

    Privacy screen navigation arrow color fix

--- screens/PrivacyScreen.js
+++ screens/PrivacyScreen.js
@@ -1,109 +1,124 @@
 import React, {Component} from 'react';
-import {FlatList, ScrollView, Text, StyleSheet} from 'react-native';
+import {FlatList, ScrollView, Text, StyleSheet, View} from 'react-native';
 
 class PrivacyScreen extends Component {
     render() {
         return (
-            <ScrollView>
-                <Text style={styles.sectionHeader}>Privacy Policy</Text>
-                <Text style={styles.itemText}>
+            <ScrollView vertical>
+                <View>
+                    <Text style={styles.sectionHeader}>Privacy Policy</Text>
+                    <Text style={styles.itemText}>
 
-                    Erlend Eelmets built the Focus app as an Ad Supported app. This SERVICE is provided by Erlend Eelmets at no
-                    cost and is intended for use as is.
+                        Erlend Eelmets built the Focus app as an Ad Supported app. This SERVICE is provided by Erlend
+                        Eelmets at no
+                        cost and is intended for use as is.
 
-                    This page is used to inform visitors regarding my policies with the collection, use, and disclosure
-                    of Personal Information if anyone decided to use my Service.
+                        This page is used to inform visitors regarding my policies with the collection, use, and
+                        disclosure
+                        of Personal Information if anyone decided to use my Service.
 
-                    If you choose to use my Service, then you agree to the collection and use of information in relation
-                    to this policy. The Personal Information that I collect is used for providing and improving the
-                    Service. I will not use or share your information with anyone except as described in this Privacy
-                    Policy.
+                        If you choose to use my Service, then you agree to the collection and use of information in
+                        relation
+                        to this policy. The Personal Information that I collect is used for providing and improving the
+                        Service. I will not use or share your information with anyone except as described in this
+                        Privacy
+                        Policy.
 
-                    The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions, which
-                    is accessible at Focus unless otherwise defined in this Privacy Policy.
-                </Text>
+                        The terms used in this Privacy Policy have the same meanings as in our Terms and Conditions,
+                        which
+                        is accessible at Focus unless otherwise defined in this Privacy Policy.
+                    </Text>
 
-                <Text style={styles.sectionHeader}> Information Collection and Use</Text>
-                <Text style={styles.itemText}>
+                    <Text style={styles.sectionHeader}> Information Collection and Use</Text>
+                    <Text style={styles.itemText}>
 
-                    For a better experience, while using our Service, I may require you to provide us with certain
-                    personally identifiable information. The information that I request will be retained on your device
-                    and is not collected by me in any way.
+                        For a better experience, while using our Service, I may require you to provide us with certain
+                        personally identifiable information. The information that I request will be retained on your
+                        device
+                        and is not collected by me in any way.
 
-                    The app does use third party services that may collect information used to identify you.
+                        The app does use third party services that may collect information used to identify you.
 
-                    Link to privacy policy of third party service providers used by the app
-                </Text>
-                <FlatList>
-                    <Text>Google Play Services</Text>
-                    <Text>AdMob</Text>
-                </FlatList>
-                <Text style={styles.sectionHeader}>Log Data</Text>
-                <Text style={styles.itemText}>
-                    I want to inform you that whenever you use my Service, in a case of an error in the app I collect
-                    data and information (through third party products) on your phone called Log Data. This Log Data may
-                    include information such as your device Internet Protocol (“IP”) address, device name, operating
-                    system version, the configuration of the app when utilizing my Service, the time and date of your
-                    use of the Service, and other statistics.
-                </Text>
-                <Text style={styles.sectionHeader}>Cookies</Text>
-                <Text style={styles.itemText}>
+                        Link to privacy policy of third party service providers used by the app
+                    </Text>
+                    <FlatList>
+                        <Text>Google Play Services</Text>
+                        <Text>AdMob</Text>
+                    </FlatList>
+                    <Text style={styles.sectionHeader}>Log Data</Text>
+                    <Text style={styles.itemText}>
+                        I want to inform you that whenever you use my Service, in a case of an error in the app I
+                        collect
+                        data and information (through third party products) on your phone called Log Data. This Log Data
+                        may
+                        include information such as your device Internet Protocol (“IP”) address, device name, operating
+                        system version, the configuration of the app when utilizing my Service, the time and date of
+                        your
+                        use of the Service, and other statistics.
+                    </Text>
+                    <Text style={styles.sectionHeader}>Cookies</Text>
+                    <Text style={styles.itemText}>
 
-                    Cookies are files with a small amount of data that are commonly used as anonymous unique
-                    identifiers. These are sent to your browser from the websites that you visit and are stored on your
-                    device's internal memory.
+                        Cookies are files with a small amount of data that are commonly used as anonymous unique
+                        identifiers. These are sent to your browser from the websites that you visit and are stored on
+                        your
+                        device's internal memory.
 
-                    This Service does not use these “cookies” explicitly. However, the app may use third party code and
-                    libraries that use “cookies” to collect information and improve their services. You have the option
-                    to either accept or refuse these cookies and know when a cookie is being sent to your device. If you
-                    choose to refuse our cookies, you may not be able to use some portions of this Service.
+                        This Service does not use these “cookies” explicitly. However, the app may use third party code
+                        and
+                        libraries that use “cookies” to collect information and improve their services. You have the
+                        option
+                        to either accept or refuse these cookies and know when a cookie is being sent to your device. If
+                        you
+                        choose to refuse our cookies, you may not be able to use some portions of this Service.
+                    </Text><Text style={styles.sectionHeader}>
+                    Service Providers
+                </Text><Text style={styles.itemText}>
+                    I may employ third-party companies and individuals due to the following reasons:
+
+                    To facilitate our Service;
+                    To provide the Service on our behalf;
+                    To perform Service-related services; or
+                    To assist us in analyzing how our Service is used.
+
+                    I want to inform users of this Service that these third parties have access to your Personal
+                    Information. The reason is to perform the tasks assigned to them on our behalf. However, they are
+                    obligated not to disclose or use the information for any other purpose.
                 </Text><Text style={styles.sectionHeader}>
-                Service Providers
-            </Text><Text style={styles.itemText}>
-                I may employ third-party companies and individuals due to the following reasons:
+                    Security
+                </Text><Text style={styles.itemText}>
+                    I value your trust in providing us your Personal Information, thus we are striving to use
+                    commercially acceptable means of protecting it. But remember that no method of transmission over the
+                    internet, or method of electronic storage is 100% secure and reliable, and I cannot guarantee its
+                    absolute security.
+                </Text><Text style={styles.sectionHeader}>
+                    Links to Other Sites
+                </Text><Text style={styles.itemText}>
+                    This Service may contain links to other sites. If you click on a third-party link, you will be
+                    directed to that site. Note that these external sites are not operated by me. Therefore, I strongly
+                    advise you to review the Privacy Policy of these websites. I have no control over and assume no
+                    responsibility for the content, privacy policies, or practices of any third-party sites or services.
+                </Text><Text style={styles.sectionHeader}>
+                    Children’s Privacy
+                </Text><Text style={styles.itemText}>
+                    These Services do not address anyone under the age of 13. I do not knowingly collect personally
+                    identifiable information from children under 13. In the case I discover that a child under 13 has
+                    provided me with personal information, I immediately delete this from our servers. If you are a
+                    parent or guardian and you are aware that your child has provided us with personal information,
+                    please contact me so that I will be able to do necessary actions.
+                </Text><Text style={styles.sectionHeader}>
+                    Changes to This Privacy Policy
+                </Text><Text style={styles.itemText}>
+                    I may update our Privacy Policy from time to time. Thus, you are advised to review this page
+                    periodically for any changes. I will notify you of any changes by posting the new Privacy Policy on
+                    this page. These changes are effective immediately after they are posted on this page.
+                </Text><Text style={styles.sectionHeader}>
+                    Contact Us
+                </Text><Text style={styles.itemText}>
+                    If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me at .
 
-                To facilitate our Service;
-                To provide the Service on our behalf;
-                To perform Service-related services; or
-                To assist us in analyzing how our Service is used.
-
-                I want to inform users of this Service that these third parties have access to your Personal
-                Information. The reason is to perform the tasks assigned to them on our behalf. However, they are
-                obligated not to disclose or use the information for any other purpose.
-            </Text><Text style={styles.sectionHeader}>
-                Security
-            </Text><Text style={styles.itemText}>
-                I value your trust in providing us your Personal Information, thus we are striving to use
-                commercially acceptable means of protecting it. But remember that no method of transmission over the
-                internet, or method of electronic storage is 100% secure and reliable, and I cannot guarantee its
-                absolute security.
-            </Text><Text style={styles.sectionHeader}>
-                Links to Other Sites
-            </Text><Text style={styles.itemText}>
-                This Service may contain links to other sites. If you click on a third-party link, you will be
-                directed to that site. Note that these external sites are not operated by me. Therefore, I strongly
-                advise you to review the Privacy Policy of these websites. I have no control over and assume no
-                responsibility for the content, privacy policies, or practices of any third-party sites or services.
-            </Text><Text style={styles.sectionHeader}>
-                Children’s Privacy
-            </Text><Text style={styles.itemText}>
-                These Services do not address anyone under the age of 13. I do not knowingly collect personally
-                identifiable information from children under 13. In the case I discover that a child under 13 has
-                provided me with personal information, I immediately delete this from our servers. If you are a
-                parent or guardian and you are aware that your child has provided us with personal information,
-                please contact me so that I will be able to do necessary actions.
-            </Text><Text style={styles.sectionHeader}>
-                Changes to This Privacy Policy
-            </Text><Text style={styles.itemText}>
-                I may update our Privacy Policy from time to time. Thus, you are advised to review this page
-                periodically for any changes. I will notify you of any changes by posting the new Privacy Policy on
-                this page. These changes are effective immediately after they are posted on this page.
-            </Text><Text style={styles.sectionHeader}>
-                Contact Us
-            </Text><Text style={styles.itemText}>
-                If you have any questions or suggestions about my Privacy Policy, do not hesitate to contact me at .
-
-            </Text>
+                </Text>
+                </View>
             </ScrollView>
         );
     }
@@ -118,6 +133,7 @@ PrivacyScreen.navigationOptions = {
     headerTitleStyle: {
         color: '#E1E4F3'
     },
+    headerTintColor:'#E1E4F3',
 };
 
 const styles = StyleSheet.create({
